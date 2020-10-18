e685037fede0fd0fe3239666e366865b79d1c4f8
Omar Negron <omarnegron92@hotmail.com>
2020-10-18 19:24:47 -0400

    Hello world

--- Queues/src/LinkedDeque.java
+++ Queues/src/LinkedDeque.java
@@ -2,7 +2,7 @@ import java.util.Iterator;
 
 public class LinkedDeque<Item> implements Iterable<Item> {
 	
-	private Node first, last;
+	private Node first = null, last = null;
 	
 	private class Node {
 		Item item;
@@ -11,13 +11,13 @@ public class LinkedDeque<Item> implements Iterable<Item> {
 	
 	// construct an empty deque
 	public <T> LinkedDeque(){
-		first = null;
-		last = null;
+
+		System.out.println("Hello World!");
 	}
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		LinkedDeque<Integer> d = new LinkedDeque<Integer>();
+		
 	}
 
 	// return an iterator over items in order from front to back
